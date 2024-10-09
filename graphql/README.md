ef02737c9fc91067f45d5bb986ec3ba16599ec30
danhhuynh25029 <danhhuynh250@gmail.com>
2024-10-09 23:55:02 +0700

    feat: use graphql

---
+++ graphql/README.md
@@ -0,0 +1,8 @@
+* Run command to init source graphql
+```
+go run github.com/99designs/gqlgen init
+```
+* Run command to generate source graphql
+```
+go run github.com/99designs/gqlgen generate
+```
---
+++ graphql/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"graph/database"
+	"graph/graph"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	// establish connection
+	database.ConnectDB()
+	// create db
+	database.CreateDB()
+	// migrate the db with Post model
+	database.MigrateDB()
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		Database: database.DBInstance,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
